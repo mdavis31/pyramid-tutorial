10f92ba9f2549abae96f7fbaa98451c1126f7785
Michael Davis <mdavis@parrsridge.com>
2020-09-23 22:35:53 +0000

    branch commit

--- 05_views/tutorial/__init__.py
+++ 05_views/tutorial/__init__.py
@@ -1,9 +1,10 @@
 from pyramid.config import Configurator
 
-def main(global_config, **settings):
+def main (global_config, **settings):
     print ("in here2")
-    config = Configurator(settings=settings)
+    config = Configurator (settings=settings)
     config.add_route ('home', '/')
     config.add_route ('hello', '/howdy')
+    config.add_route ('hi', '/hi')
     config.scan ('.views')
     return config.make_wsgi_app ()
--- 05_views/tutorial/views.py
+++ 05_views/tutorial/views.py
@@ -8,3 +8,8 @@ def home (request):
 @view_config (route_name='hello')
 def hello (request):
     return Response('<body><h1>Hello World!</h1><a href="/">Go Back</a></body>')
+
+@view_config (route_name='hi')
+def hi (request):
+    return Response('<body><h1>Hello World!</h1><a href="/">Go Back</a></body>')
+
